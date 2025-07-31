94f6e3f6618b903ecd78c630cbea4b215a77e31b
clkm75 <calvin@o3cs.my>
2025-07-31 10:20:40 +0800

    Create Mess1.ps1

---
+++ Mess1.ps1
@@ -0,0 +1,2 @@
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+[System.Windows.Forms.MessageBox]::Show("This is a message from IT. Please save your work.", "Notice from IT", 'OK', 'Information')
